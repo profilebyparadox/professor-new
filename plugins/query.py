e5c1f425794d5778e7cf4a08a3171e6fb700aef2
Harsh2534 <121288789+Harsh2534@users.noreply.github.com>
2023-05-05 22:23:37 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -620,7 +620,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "source":
         buttons = [[
-            InlineKeyboardButton('SOURCE CODE', url='https://github.com/MrMKN/PROFESSOR-BOT')
+            InlineKeyboardButton('SOURCE CODE', url='https://t.me/MoviesUpdate_07')
             ],[
             InlineKeyboardButton('👩‍🦯 Back', callback_data='about')
         ]]
