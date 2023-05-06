3a7c34620d8a51edbfd4b86decbfbbfd493c744f
Harsh2534 <121288789+Harsh2534@users.noreply.github.com>
2023-05-06 13:24:02 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -424,7 +424,7 @@ def humanbytes(size):
 
 async def get_shortlink(link):
 
-    url = f' https://mdiskshortner.link/api'
+    url = f' https://omegalinks.in/api'
     params = {
       'api': SHORT_API,
       'url': link,
